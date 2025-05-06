8de111a0edb4b1dbb74fea6bbf95b377c313e110
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-06 15:27:00 +0200

    [UPD] Complete cards with information passed via props

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,7 +1,9 @@
 const ComicCard = ({ img, series }) => {
     return (
-        <div className="card">
-            <img src={img} alt="" />
+        <div className="comic-card">
+            <div className="card-image">
+                <img src={img} alt="" />
+            </div>
             <p>{series}</p>
         </div>
     )
--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -4,7 +4,20 @@ import ComicCard from './ComicCard'
 const ListComics = () => {
     return (
         <div className="container">
-            <ComicCard img={`https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY`} series={`Action Comics`} />
+            <div className="comic-cards">
+                <ComicCard img={comics[0].thumb} series={comics[0].series} />
+                <ComicCard img={comics[1].thumb} series={comics[1].series} />
+                <ComicCard img={comics[2].thumb} series={comics[2].series} />
+                <ComicCard img={comics[3].thumb} series={comics[3].series} />
+                <ComicCard img={comics[4].thumb} series={comics[4].series} />
+                <ComicCard img={comics[5].thumb} series={comics[5].series} />
+                <ComicCard img={comics[6].thumb} series={comics[6].series} />
+                <ComicCard img={comics[7].thumb} series={comics[7].series} />
+                <ComicCard img={comics[8].thumb} series={comics[8].series} />
+                <ComicCard img={comics[9].thumb} series={comics[9].series} />
+                <ComicCard img={comics[10].thumb} series={comics[10].series} />
+                <ComicCard img={comics[11].thumb} series={comics[11].series} />
+            </div>
         </div>
     )
 }
