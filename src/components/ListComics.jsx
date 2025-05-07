f61e99e6f08fdd7c253313ad84f0fbc8f4fb0433
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-07 14:40:56 +0200

    [DONE] implement array iteration for comic rendering

--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -7,18 +7,9 @@ const ListComics = () => {
         <div className="container wrap p-relative">
             <span id='series'>current series</span>
             <div className="comic-cards">
-                <ComicCard img={comics[0].thumb} series={comics[0].series} />
-                <ComicCard img={comics[1].thumb} series={comics[1].series} />
-                <ComicCard img={comics[2].thumb} series={comics[2].series} />
-                <ComicCard img={comics[3].thumb} series={comics[3].series} />
-                <ComicCard img={comics[5].thumb} series={comics[4].series} />
-                <ComicCard img={comics[5].thumb} series={comics[5].series} />
-                <ComicCard img={comics[6].thumb} series={comics[6].series} />
-                <ComicCard img={comics[7].thumb} series={comics[7].series} />
-                <ComicCard img={comics[8].thumb} series={comics[8].series} />
-                <ComicCard img={comics[9].thumb} series={comics[9].series} />
-                <ComicCard img={comics[10].thumb} series={comics[10].series} />
-                <ComicCard img={comics[11].thumb} series={comics[11].series} />
+                {comics.map(comic => (
+                    <ComicCard img={comic.thumb} series={comic.series} />
+                ))}
             </div>
             <Button />
         </div>
